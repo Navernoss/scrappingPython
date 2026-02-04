e77f37333ce442058755ba7fd31195c71bc75b62
Navernoss <chuklinovivan@yandex.com>
2026-02-04 21:35:07 +0500

    solght name

--- bs4/json/task3.py
+++ bs4/json/task3.py
@@ -1,22 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
+import time
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 result_json = []
 url_p = 'https://parsinger.ru/html/index3_page_1.html'
-response_p = requests.get(url=url_p)
+response_p = requests.get(url=url_p, timeout=5 ,headers=headers)
 soup_p = BeautifulSoup(response_p.text,'lxml')
 count_of_pages = int(len(soup_p.find('div',class_='pagen').find_all('a')))
 list_of_links = []
+time.sleep(5)
 for i in range(1,count_of_pages + 1):
     url = f'https://parsinger.ru/html/index3_page_{i}.html'
-    response = requests.get(url=url)
+    response = requests.get(url=url, timeout=10, headers=headers)
     soup = BeautifulSoup(response.text,'lxml')
     links_of_page = [f'https://parsinger.ru/html/{link['href']}' for link in soup.find_all('a',class_='name_item')]
     list_of_links.extend(links_of_page)
+    time.sleep(5)
 for link in list_of_links:
     url = link
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+    
     response = requests.get(url=url, timeout=20, headers=headers)
     response.encoding='utf-8'
     soup = BeautifulSoup(response.text,'lxml')
@@ -42,6 +45,7 @@ for link in list_of_links:
         'in_stock': in_stock,
         'price': price
     })
+    time.sleep(3)
 
 with open ('res.json','w',encoding='utf-8-sig') as file:
     json.dump(result_json,file,indent=4, ensure_ascii=False)
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests 
 from bs4 import BeautifulSoup
 
-url = 'https://solght.ru/about/teachers/pedagogicheskiy-sostav/ankushina-marina-teodorovna/'
+url = 'https://solght.ru/about/teachers/pedagogicheskiy-sostav/antipina-mariya-vladimirovna/'
 
 response = requests.get(url=url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text,'lxml')
-name = soup.find('div',class_="col-md-4 left-column").find('div',class_="class=col-md-8")
-print(soup)
+name = soup.find('div',class_="wp-section").find('div',class_="row").find('div',class_="col-md-8").find('h1').text
+print(name)
